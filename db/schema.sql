1ce9b45507ceb3c9fc5ea518775b65156558099d
Aditya Gumilar <Aditya.Gumilar@bni.co.id>
2025-12-29 11:37:49 +0700

    Milestone 2: fixing pyproject

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,9 @@ CREATE TABLE IF NOT EXISTS events (
   available_stock INT NOT NULL CHECK (available_stock >= 0),
   reserved_count INT NOT NULL CHECK (reserved_count >= 0)
 );
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY,
+  event_id TEXT NOT NULL REFERENCES events(id),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
